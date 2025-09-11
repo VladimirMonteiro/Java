b793a8dac805b32b6614e6f7eea92a046a826a7d
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2025-09-11 20:46:26 -0300

    add modules

--- JavaBasic/src/main/java/org/example/Main.java
+++ JavaBasic/src/main/java/org/example/Main.java
@@ -1,17 +1,17 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<String> names = List.of("Vladimir", "Pedro", "Nora", "Richerth");
+        Map<Integer, String> namesMap = new HashMap<>();
+        namesMap.put(1, "Lorenço");
+        namesMap.put(2, "Dias");
+        namesMap.put(3, "Carla");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        namesMap.forEach((index, name) -> System.out.println(index + " " + name ));
     }
 }
