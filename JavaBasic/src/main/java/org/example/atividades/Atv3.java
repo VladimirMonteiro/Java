68bc5d4c830a38c7877e48606dbd9c0eee6dc934
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2025-09-12 18:26:17 -0300

    atv3 completed

---
+++ JavaBasic/src/main/java/org/example/atividades/Atv3.java
@@ -0,0 +1,10 @@
+package org.example.atividades;
+
+/*Declare uma variável do tipo char (letra) e uma variável do tipo String (palavra). Atribua valores a essas variáveis e concatene-as em uma mensagem.*/
+public class Atv3 {
+    public static void main(String[] args) {
+        char sexo = 'M';
+        String text = "Meu Nome é Roberto.";
+        System.out.println(text + " Meu sexo é " + sexo);
+    }
+}
