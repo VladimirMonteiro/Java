74c3b248bace9d2c6e0ea8ae911842d48cc81a21
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2025-09-12 19:47:09 -0300

    Class Scanner

---
+++ JavaBasic/src/main/java/org/example/atividades/Leituna.java
@@ -0,0 +1,27 @@
+package org.example.atividades;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Leituna {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do seu filme preferido: ");
+        String filme = scanner.nextLine();
+
+        System.out.print("Informe o ano de lançamento: ");
+        int anoLancamento = scanner.nextInt();
+
+        System.out.println("Diga sua avaliação para o filme");
+        double avaliacao = scanner.nextDouble();
+
+        System.out.println("filme");
+        System.out.println("anoDeLancamento");
+        System.out.println("avaliacao");
+
+
+
+    }
+}
