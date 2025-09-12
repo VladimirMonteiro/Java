fe5f0335cdfda23a391426ea18e1e5d9c89014b5
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2025-09-12 18:23:01 -0300

    atv2 completed

---
+++ JavaBasic/src/main/java/org/example/atividades/Atv2.java
@@ -0,0 +1,10 @@
+package org.example.atividades;
+
+/*Declare uma variável do tipo double e uma variável do tipo int. Faça o casting da variável double para int e imprima o resultado.*/
+public class Atv2 {
+    public static void main(String[] args) {
+        double x = 25.5;
+        int y = (int) x;
+        System.out.println(y);
+    }
+}
