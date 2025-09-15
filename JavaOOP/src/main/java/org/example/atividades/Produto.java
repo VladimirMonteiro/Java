b82e53ed799d061b47ab157f0200ec2b8a71eb94
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2025-09-15 19:56:32 -0300

    atv3 completed

---
+++ JavaOOP/src/main/java/org/example/atividades/Produto.java
@@ -0,0 +1,31 @@
+package org.example.atividades;
+
+/*
+Desenvolva uma classe Produto com os atributos privados nome e preco.
+Utilize métodos getters e setters para acessar e modificar esses atributos.
+Adicione um método aplicarDesconto que recebe um valor percentual e reduz o preço do produto.
+*/
+public class Produto {
+    private String nome;
+    private double preco;
+
+    public void aplicarDesconto(double porcentual) {
+        preco -= (porcentual/ 100) * preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
