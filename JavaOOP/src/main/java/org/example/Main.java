a63d1bc30ad598af91739d967509a195fd7be83d
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2025-09-15 20:08:55 -0300

    atv4 completed

--- JavaOOP/src/main/java/org/example/Main.java
+++ JavaOOP/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 
+import org.example.atividades.Aluno;
 import org.example.atividades.IdadePessoa;
 import org.example.atividades.Produto;
 
@@ -18,5 +19,12 @@ public class Main {
         produto.setPreco(100);
         produto.aplicarDesconto(10);
         System.out.println(produto.getPreco());
+
+        // Atividade 4
+        System.out.println("Resultado atividade 4");
+        Aluno aluno = new Aluno();
+        aluno.setNota1(10);
+        aluno.setNota2(10);
+        System.out.println(aluno.calcularMedia());
     }
 }
