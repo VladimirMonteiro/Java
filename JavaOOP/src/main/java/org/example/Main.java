3e49e62516c7caafb55f4712ec9be3d12bc9690c
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2025-09-15 19:56:42 -0300

    atv2 atv3 completed

--- JavaOOP/src/main/java/org/example/Main.java
+++ JavaOOP/src/main/java/org/example/Main.java
@@ -1,11 +1,22 @@
 package org.example;
 
 
-
+import org.example.atividades.IdadePessoa;
+import org.example.atividades.Produto;
 
 public class Main {
     public static void main(String[] args) {
+        // Atividade 2
+        System.out.println("Resultado atividade 2");
+        IdadePessoa ip = new IdadePessoa();
+        ip.setIdade(9);
+        ip.verificarIdade();
 
-
+        // Atividade 3
+        System.out.println("Resultado ativdade 3");
+        Produto produto = new Produto();
+        produto.setPreco(100);
+        produto.aplicarDesconto(10);
+        System.out.println(produto.getPreco());
     }
 }
