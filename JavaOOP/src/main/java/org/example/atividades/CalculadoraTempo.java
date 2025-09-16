ea2ff22f76acfbde94667d11856716b61a3102a1
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2025-09-16 18:15:38 -0300

    polimorfismo

---
+++ JavaOOP/src/main/java/org/example/atividades/CalculadoraTempo.java
@@ -0,0 +1,21 @@
+package org.example.atividades;
+
+public class CalculadoraTempo {
+    private int tempoTotal;
+
+    public int getTempoTotal() {
+        return tempoTotal;
+    }
+
+//    public void inclui(Filme f) {
+//        tempoTotal += f.getDuracaoEmMinutos();
+//    }
+//
+//    public void inclui(Serie s) {
+//        tempoTotal += s.getDuracaoEmMinutos();
+//    }
+
+    public void inclui(Titulo t) {
+        tempoTotal += t.getDuracaoEmMinutos();
+    }
+}
--- JavaOOP/src/main/java/org/example/atividades/Filme.java
+++ JavaOOP/src/main/java/org/example/atividades/Filme.java
@@ -1,58 +1,13 @@
 package org.example.atividades;
 
-public class Filme {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+public class Filme extends Titulo {
+    String diretor;
 
-    public String getNome() {
-        return nome;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public double getSomaDasAvaliacoes() {
-        return somaDasAvaliacoes;
-    }
-
-    public void setSomaDasAvaliacoes(double somaDasAvaliacoes) {
-        this.somaDasAvaliacoes = somaDasAvaliacoes;
-    }
-
-    public int getTotalDeAvaliacoes() {
-        return totalDeAvaliacoes;
-    }
-
-    public void setTotalDeAvaliacoes(int totalDeAvaliacoes) {
-        this.totalDeAvaliacoes = totalDeAvaliacoes;
-    }
-
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
