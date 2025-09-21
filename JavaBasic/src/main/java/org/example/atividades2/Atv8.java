d9489f202a9ba55529c76a1f0a0fe334ac98f88e
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2025-09-21 12:05:52 -0300

    Atividade 8 finalizada

---
+++ JavaBasic/src/main/java/org/example/atividades2/Atv8.java
@@ -0,0 +1,12 @@
+package org.example.atividades2;
+
+
+/*Crie um programa que realize a conversão para dólares usando a taxa de câmbio e exiba o valor convertido.*/
+public class Atv8 {
+    public static void main(String[] args) {
+        double valorReais = 451.50;
+        double valorDolares = valorReais / 5.25;
+        System.out.println("O valor em dólares é: US$ " + valorDolares);
+
+    }
+}
