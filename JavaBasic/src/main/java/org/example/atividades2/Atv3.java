ec0e56b044a9b05a7ee718e8c63e35a778d06da9
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2025-09-21 11:37:40 -0300

    atvidade 3 finalizada

---
+++ JavaBasic/src/main/java/org/example/atividades2/Atv3.java
@@ -0,0 +1,12 @@
+package org.example.atividades2;
+
+/*Calcule a média de 3 notas de um aluno.*/
+public class Atv3 {
+    public static void main(String[] args) {
+        double nota1 = 7.5;
+        double nota2 = 8.0;
+        double nota3 = 9.0;
+
+        System.out.println("A média das notas é: " + (nota1 + nota2 + nota3) / 3);
+    }
+}
