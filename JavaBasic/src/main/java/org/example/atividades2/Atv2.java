8f35b9e9fbe635ac3a933749570643807a2998db
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2025-09-21 11:34:22 -0300

    atividade 2 finalizada

---
+++ JavaBasic/src/main/java/org/example/atividades2/Atv2.java
@@ -0,0 +1,14 @@
+package org.example.atividades2;
+
+
+/*
+Converta um número double para int.
+*/
+public class Atv2 {
+    public static void main(String[] args) {
+
+        double valorDouble = 19.5;
+        int valorInteiro = (int) valorDouble;
+        System.out.println("O valor inteiro do produto é: " + valorInteiro);
+    }
+}
